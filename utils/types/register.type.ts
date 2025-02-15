4b8ee0c335148f746b3e1c3d3ae05585669fa4cf
AJ APARICIO <aparicio.anthonyjohn@gmail.com>
2025-02-15 19:40:04 +0800

    Migrate to zod validation

--- utils/types/register.type.ts
+++ utils/types/register.type.ts
@@ -1,10 +1,14 @@
-export interface RegisterPayload {
-  access_token: string;
-  name: string;
-}
+import { z } from "zod";
 
-export interface RegisterFormData {
-  email: string;
-  name: string;
-  password: string;
-}
+export const RegisterPayloadSchema = z.object({
+  access_token: z.string().jwt(),
+  name: z.string(),
+});
+export type RegisterPayload = z.infer<typeof RegisterPayloadSchema>;
+
+export const RegisterFormDataSchema = z.object({
+  email: z.string().email(),
+  name: z.string().min(4, "Name must be 4 or more characters"),
+  password: z.string().min(8, "Password must be 8 or more characters"),
+});
+export type RegisterFormData = z.infer<typeof RegisterFormDataSchema>;
